c52386da68e9dd0da7f02bc7160d65a344a4842d
Madhur Anand <anandmadhurrkm7@gmail.com>
2025-06-16 20:28:26 +0530

    path changed

--- src/searchBox.jsx
+++ src/searchBox.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import "./searchBox.css"
+import "./searchbox.css"
 
 function SearchBox({ updateInfo }) {
     const [city, setCity] = useState("");
